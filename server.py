ca9395d50bee3e6beff3050b4fdbe46194ff872c
Matiyas-h <mattsis71@icloud.com>
2024-07-04 22:26:03 +0300

    all

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse
-
+from loguru import logger
 from bot import run_bot
 
 app = FastAPI()
@@ -55,19 +55,37 @@ async def initiate_outbound_call(background_tasks: BackgroundTasks, to_number: s
 @app.post('/outbound_call_handler')
 async def outbound_call_handler():
     response = VoiceResponse()
+    response.say("Hello, this is an AI assistant calling. Please wait while I connect you.")
     response.connect().stream(url=f"wss://{os.environ.get('RENDER_EXTERNAL_URL')}/ws")
+    logger.info(f"Outbound call TwiML: {response}")
     return HTMLResponse(content=str(response), media_type="application/xml")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    logger.info("WebSocket connection attempt")
     await websocket.accept()
-    start_data = websocket.iter_text()
-    await start_data.__anext__()
-    call_data = json.loads(await start_data.__anext__())
-    print(call_data, flush=True)
-    stream_sid = call_data['start']['streamSid']
-    print("WebSocket connection accepted")
-    await run_bot(websocket, stream_sid)
+    logger.info("WebSocket connection accepted")
+    
+    try:
+        start_data = websocket.iter_text()
+        initial_message = await start_data.__anext__()
+        logger.info(f"Initial WebSocket message: {initial_message}")
+        
+        call_data = json.loads(await start_data.__anext__())
+        logger.info(f"Call data: {call_data}")
+        
+        stream_sid = call_data['start']['streamSid']
+        logger.info(f"Stream SID: {stream_sid}")
+        
+        # Determine if this is an outbound call
+        is_outbound = call_data.get('start', {}).get('outbound') == 'true'
+        logger.info(f"Is outbound call: {is_outbound}")
+        
+        await run_bot(websocket, stream_sid, is_outbound)
+    except Exception as e:
+        logger.error(f"Error in websocket_endpoint: {str(e)}")
+    finally:
+        logger.info("WebSocket connection closed")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8765))
